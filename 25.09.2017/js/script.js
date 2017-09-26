1c137a515181729f9c1f50babf77e8cedceac260
Saidaliyev7 <mrsaidaliyev@hotmail.com>
2017-09-26 21:15:47 +0400

    Add files via upload

--- 25.09.2017/js/script.js
+++ 25.09.2017/js/script.js
@@ -1,5 +1,32 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $('#firstCarousel').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+  }); 
+  $('#secondCarousel').owlCarousel({
+    items: 3,
+    loop: true,
+    margin:3,
+    dots:false,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:3
+        }
+    }
+  })
 
+  $("#secondCarousel .case-content .hover").mouseover(function(){
+    $(this).addClass("hoverActive");
+  })
+  $("#secondCarousel .case-content .hover").mouseout(function(){
+    $(this).removeClass("hoverActive");
+  })
 
-  });
+});
